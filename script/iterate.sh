2a40ad8e53145c7718829a0ffb27bc63f80dd587
Jon Bardin <diclophis@gmail.com>
2021-06-15 00:51:57 -0400

    bits

--- script/iterate.sh
+++ script/iterate.sh
@@ -3,9 +3,10 @@
 set -e
 set -x
 
+BIND_MOUNT_PUBLIC_DIR="--mount type=bind,source=$(pwd)/buffer,target=/var/tmp/app/buffer"
+
 docker build -f Dockerfile.jruby -t bxpk:latest-jruby . 2>&1 > /dev/null
+time docker run --cpus=10 -it --rm bxpk:latest-jruby bin/bxpk
 
 docker build -f Dockerfile -t bxpk:latest . 2>&1 > /dev/null
-
-time docker run --cpus=2 -it --rm bxpk:latest-jruby bin/bxpk
-time docker run --cpus=2 -it --rm bxpk:latest bin/bxpk
+time docker run --cpus=10 -it --rm bxpk:latest bin/bxpk
