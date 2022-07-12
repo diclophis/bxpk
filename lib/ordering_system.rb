e3aa3ecb675b97409b2fb2da29d5bd84d760a5b6
Jon Bardin <diclophis@gmail.com>
2022-07-12 11:41:21 +0000

    more concurrency bits

--- lib/ordering_system.rb
+++ lib/ordering_system.rb
@@ -1,5 +1,6 @@
 class OrderingSystem
-  def initialize(&the_compare_proc)
+  def initialize(semaphore = Mutex.new, &the_compare_proc)
+    @semaphore = semaphore
     @compare_proc = the_compare_proc
     reset!
   end
@@ -9,32 +10,36 @@ class OrderingSystem
   end
 
   def reset!
-    @items = []
-    @item_components = {}
-    @i = 0
+    @semaphore.synchronize {
+      @items = []
+      @item_components = {}
+      @i = 0
+    }
   end
 
   def order_by(key, component)
-    @item_components[key] = component
+    @semaphore.synchronize {
+      @item_components[key] = component
 
-    ## Iterate through the length of the array, push into method
-    if @i == 0
-      @items[@i] = key
-    else
-      ## beginning with the second element items[i]
-      j = @i - 1
+      ## Iterate through the length of the array, push into method
+      if @i == 0
+        @items[@i] = key
+      else
+        ## beginning with the second element items[i]
+        j = @i - 1
 
-      # If element to left of key is larger then
-      # move it one position over at a time
-      while j >= 0 and @compare_proc.call(@item_components[@items[j]], component)
-        @items[j + 1] = @items[j]
-        j = j - 1
+        # If element to left of key is larger then
+        # move it one position over at a time
+        while j >= 0 and @compare_proc.call(@item_components[@items[j]], component)
+          @items[j + 1] = @items[j]
+          j = j - 1
+        end
+
+        # Update key position
+        @items[j+1] = key
       end
 
-      # Update key position
-      @items[j+1] = key
-    end
-
-    @i += 1
+      @i += 1
+    }
   end
 end
